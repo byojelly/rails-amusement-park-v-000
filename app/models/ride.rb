493c1acf972b872c1a857891ca0feef4095f2110
Alex <github email address>
2018-03-22 18:02:14 -0400

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,9 @@
 class Ride < ActiveRecord::Base
-  # write associations here
+  # write associations here   rspec spec/models/ride_spec.rb
+  belongs_to :user
+  belongs_to :attraction
+
+  def take_ride
+        binding.pry
+  end
 end
--- db/migrate/20140703140419_create_rides.rb
+++ db/migrate/20140703140419_create_rides.rb
@@ -1,3 +1,10 @@
 class CreateRides < ActiveRecord::Migration
   # Write your migrations here
+  # this is a joins table so you can have many through many
+  def change
+    create_table :rides do |t|
+        t.integer :user_id
+        t.integer :attraction_id
+    end
+  end
 end
