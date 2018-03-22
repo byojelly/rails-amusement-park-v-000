1851193be0d84e0315c409ffcc5df923025d4eb6
Alex <github email address>
2018-03-22 17:23:37 -0400

    Done.

--- db/migrate/20140703140340_create_users.rb
+++ db/migrate/20140703140340_create_users.rb
@@ -1,3 +1,20 @@
 class CreateUsers < ActiveRecord::Migration
   # Write your migrations here
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :password_digest
+t.integer :nausea
+t.integer :nausea_rating
+t.integer :happiness
+t.integer :happiness_rating
+t.integer :tickets
+t.integer :height
+t.boolean :admin
+
+
+
+      t.timestamps null: false
+    end
+  end
 end
